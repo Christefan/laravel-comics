85ac9a1632872e33a4a1d2307791449b66be6678
Christefan <chriscapod7@gmail.com>
2022-06-24 17:20:00 +0200

    Funzionamento URL

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,53 +1,42 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
 <footer>
     <div class="footer-container">
         <div class="footer-nav">
-        <ul>
-            <li><h2>DC COMICS</h2></li>
-            <li><a href="#"> Caracters </a></li>
-            <li><a href="#"> Comics </a></li>
-            <li><a href="#"> Movies </a></li>
-            <li><a href="#"> Tv </a></li>
-            <li><a href="#"> Games </a></li>
-            <li><a href="#"> Videos </a></li>
-            <li><a href="#"> News </a></li>
+            <ul>
+                <li><h2>DC COMICS</h2></li>
+                <li><a href="#"> Caracters </a></li>
+                <li><a href="#"> Comics </a></li>
+                <li><a href="#"> Movies </a></li>
+                <li><a href="#"> Tv </a></li>
+                <li><a href="#"> Games </a></li>
+                <li><a href="#"> Videos </a></li>
+                <li><a href="#"> News </a></li>
 
-            <li><h2>SHOP</h2></li>
-            <li><a href="#"> Shop DC </a></li>
-            <li><a href="#"> Shop DC Collectibles </a></li>
-        </ul>
-        <ul>
-            <li><h2>DC</h2></li>
-            <li><a href="#"> Terms of Use </a></li>
-            <li><a href="#"> Privacy Policy </a></li>
-            <li><a href="#"> Ad Choices </a></li>
-            <li><a href="#"> Advertising </a></li>
-            <li><a href="#"> Jobs </a></li>
-            <li><a href="#"> Subscriptions </a></li>
-            <li><a href="#"> Talent Workshops </a></li>
-            <li><a href="#"> CPSC Certificates </a></li>
-            <li><a href="#"> Ratings </a></li>
-            <li><a href="#"> Shop Help </a></li>
-            <li><a href="#"> Contact Us </a></li>
-        </ul>
-        <ul>
-            <li><h2>SITES</h2></li>
-            <li><a href="#"> DC </a></li>
-            <li><a href="#"> MAD Magazine </a></li>
-            <li><a href="#"> DC Kid </a></li>
-            <li><a href="#"> DC Universe </a></li>
-            <li><a href="#"> DC Power Visa </a></li>
-        </ul>
+                <li><h2>SHOP</h2></li>
+                <li><a href="#"> Shop DC </a></li>
+                <li><a href="#"> Shop DC Collectibles </a></li>
+            </ul>
+            <ul>
+                <li><h2>DC</h2></li>
+                <li><a href="#"> Terms of Use </a></li>
+                <li><a href="#"> Privacy Policy </a></li>
+                <li><a href="#"> Ad Choices </a></li>
+                <li><a href="#"> Advertising </a></li>
+                <li><a href="#"> Jobs </a></li>
+                <li><a href="#"> Subscriptions </a></li>
+                <li><a href="#"> Talent Workshops </a></li>
+                <li><a href="#"> CPSC Certificates </a></li>
+                <li><a href="#"> Ratings </a></li>
+                <li><a href="#"> Shop Help </a></li>
+                <li><a href="#"> Contact Us </a></li>
+            </ul>
+            <ul>
+                <li><h2>SITES</h2></li>
+                <li><a href="#"> DC </a></li>
+                <li><a href="#"> MAD Magazine </a></li>
+                <li><a href="#"> DC Kid </a></li>
+                <li><a href="#"> DC Universe </a></li>
+                <li><a href="#"> DC Power Visa </a></li>
+            </ul>
         </div>
     </div>
-    </footer>    
-</body>
-</html>
+</footer>
