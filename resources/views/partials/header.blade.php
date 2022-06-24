7ddd7ed65c136d0c527114993edd58428682dc4d
Christefan <chriscapod7@gmail.com>
2022-06-24 15:59:23 +0200

    first commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="header-container">
+    <div class="logo">
+    <img src="../assets/img/dc-logo.png" alt="" />
+    </div>
+    <div class="list-header">
+    <ul>
+        <!-- <li v-for="(items, index) in ArrText" :key="index">
+        <a href="">{{ items.text }}</a>
+        </li> -->
+        <li>CHARACTERS</li>
+        <li>COMICS</li>
+        <li>MOVIES</li>
+        <li>TV</li>
+        <li>GAMES</li>
+        <li>COLLECTIBILES</li>
+        <li>VIDEOS</li>
+        <li>FANS</li>
+        <li>NEWS</li>
+        <li>SHOP</li>
+    </ul>
+    </div>
+</div>
+</template>
+</body>
+</html>
